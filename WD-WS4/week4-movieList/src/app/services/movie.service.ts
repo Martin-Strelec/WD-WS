d2f28434cbdc11585b3de1e3c508136be5ed8c9b
Martin-Strelec <m.strelec2021@gmail.com>
2025-02-11 23:10:33 +0000

    Modified movieList & added addMovie method

--- WD-WS4/week4-movieList/src/app/services/movie.service.ts
+++ WD-WS4/week4-movieList/src/app/services/movie.service.ts
@@ -8,13 +8,17 @@ export class MovieService {
   constructor() { }
 
   movieList = [
-    {id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola'},
-    {id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen brothers'},
-    {id: 3, title: 'Dial M for murder', year: '1954', director: 'Alfred Hitchcock'},
-    {id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
+    {title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola'},
+    {title: 'Millers Crossing', year: '1990', director: 'The Coen brothers'},
+    {title: 'Dial M for murder', year: '1954', director: 'Alfred Hitchcock'},
+    {title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
     ];
 
     getMovies () {
       return this.movieList;
+    }
+
+    addMovie (movietitle:string, moviedirector:string, movieyear:string) {
+      this.movieList.push({title:movietitle, director:moviedirector, year:movieyear});
     }
 }
