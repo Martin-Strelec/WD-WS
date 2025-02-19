a8a693e2a47ecb76da95b2b5be68dd3abbad4f39
Martin-Strelec <m.strelec2021@gmail.com>
2025-02-19 10:33:11 +0000

    Added eventemitter & output component

--- WD-WS5/week5-movieList/src/app/list-movies/list-movies.component.ts
+++ WD-WS5/week5-movieList/src/app/list-movies/list-movies.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieService } from '../services/movie.service';
 import { Movie } from '../movie.model';
