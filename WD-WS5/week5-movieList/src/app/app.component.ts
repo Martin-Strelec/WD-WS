b3a59bd2d634c71386b8c5f76c6ee1f0c83efbab
Martin-Strelec <m.strelec2021@gmail.com>
2025-02-19 10:53:36 +0000

    Imported Movie class & MovieDetail component & added setSelectedMovie method

--- WD-WS5/week5-movieList/src/app/app.component.ts
+++ WD-WS5/week5-movieList/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+
 import { AddMovieComponent } from "./add-movie/add-movie.component";
 import { ListMoviesComponent } from "./list-movies/list-movies.component";
+import { MovieDetailComponent } from "./movie-detail/movie-detail.component";
+
+import { Movie } from './movie.model';
 
 @Component({
   selector: 'app-root',
-  imports: [AddMovieComponent, ListMoviesComponent],
+  imports: [AddMovieComponent, ListMoviesComponent, MovieDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'week4-movieList';
+
+  public mySelectedMovie!:Movie;
+
+  setSelectedMovie(movie:Movie) {
+    this.mySelectedMovie=movie;
+  }
 }
