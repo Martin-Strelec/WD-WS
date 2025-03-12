93c2bf6490d88ae6a552fc1f458a3d89d9dbf3bb
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-12 11:07:08 +0000

    Imported CommonModule & omdbapiservice & iomdbresponse2 & added pagination methods & getmoviedetails method

--- WD-WS7/movie-finder/src/app/components/search/search.component.ts
+++ WD-WS7/movie-finder/src/app/components/search/search.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OmbdpApiService } from '../../services/ombdp-api.service';
+import { IOMDBResponse2 } from '../../omdbResponse2';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [CommonModule, ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
 
+export class SearchComponent {
+  constructor (private _omdbService:OmbdpApiService) {}
+  movieData:IOMDBResponse2|undefined;
+  currentPage:number = 1
+  maxPages:number = 0
+  errorMessage:any
+
+  getMovieDetails(movieName:string): boolean {
+    this._omdbService.getMoviesData(movieName,this.currentPage).subscribe(
+      movieData => {
+        this.movieData=movieData;
+        //console.log("Director name : " + this.movieData.Director);
+      }
+    )
+    return false;
+  }
+
+  getPreviousPage(movieName:string): boolean {
+    this.currentPage--;
+    if (this.currentPage<1)
+      this.currentPage=1;
+    this._omdbService.getMoviesData(movieName, this.currentPage).subscribe(
+      movieData => {
+        this.movieData=movieData;
+      }
+    )
+    return false;
+  }
+
+  getNextPage(movieName:string): boolean {
+    this.currentPage++;
+    this._omdbService.getMoviesData(movieName, this.currentPage).subscribe(
+      movieData => {
+        this.movieData=movieData;
+      }
+    )
+    return false;
+  }
 }
