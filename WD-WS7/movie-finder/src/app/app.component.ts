590a09c66c2b46da4cb5c3a6fb3a51061886ff02
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-12 10:15:06 +0000

    Added getMoviesDetails method & setDefaultImg method

--- WD-WS7/movie-finder/src/app/app.component.ts
+++ WD-WS7/movie-finder/src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { IOMDBResponse } from './omdbResponse';
+import { OmbdpApiService } from './services/ombdp-api.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'movie-finder';
+  title = 'Movie Finder';
+  movieData?:IOMDBResponse | undefined;
+  errorMessage:any;
+
+  constructor(private _omdbService:OmbdpApiService){}
+
+  getMovieDetails(movieName:string): boolean {
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData=movieData;
+        console.log("Director name: " + this.movieData.Director);
+
+      }
+    )
+    return false;
+  }
+
+  setDefaultImg(event: any) {
+    event.target.src = '../../public/default.png'; // Ensure the default image is in the assets folder
+  }
 }
