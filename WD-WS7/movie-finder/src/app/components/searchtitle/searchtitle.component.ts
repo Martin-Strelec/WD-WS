cdeb198b6d120fb0c79c1792e95a79980113f867
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-12 10:37:57 +0000

    Imported Common module, OMDBAPIService and IOMDB interface

--- WD-WS7/movie-finder/src/app/components/searchtitle/searchtitle.component.ts
+++ WD-WS7/movie-finder/src/app/components/searchtitle/searchtitle.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IOMDBResponse } from '../../omdbResponse';
+import { OmbdpApiService } from '../../services/ombdp-api.service';
 
 @Component({
   selector: 'app-searchtitle',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './searchtitle.component.html',
   styleUrl: './searchtitle.component.css'
 })
 export class SearchtitleComponent {
+  title = 'Movie Finder';
+  movieData?:IOMDBResponse | undefined;
+  errorMessage:any;
 
+  constructor(private _omdbService:OmbdpApiService){}
+
+  getMovieDetails(movieName:string): boolean {
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData=movieData;
+        console.log("Director name: " + this.movieData.Director);
+
+      }
+    )
+    return false;
+  }
+
+  setDefaultImg(event: any) {
+    event.target.src = '../../public/default.png'; // Ensure the default image is in the assets folder
+  }
 }
