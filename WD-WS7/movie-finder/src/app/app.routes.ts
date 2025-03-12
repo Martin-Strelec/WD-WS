1d1f93220628c84580d2d0a876840339cc353268
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-12 10:36:53 +0000

    Added about, serachtitle, search routes

--- WD-WS7/movie-finder/src/app/app.routes.ts
+++ WD-WS7/movie-finder/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SearchtitleComponent } from './components/searchtitle/searchtitle.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component:SearchtitleComponent},
+    { path: 'search', component:SearchtitleComponent},
+    { path: 'about', component:SearchtitleComponent}
+];
