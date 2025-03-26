2e75267ae42b4afbfefb61b70e3ff50d1d40169b
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-26 10:10:12 +0000

    imported Input, CarApiService, ICar, CommonModule

--- CA2/carsales/carsales-nosql/src/app/components/car/car.component.ts
+++ CA2/carsales/carsales-nosql/src/app/components/car/car.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { CarApiService } from '../../service/car-api.service';
+import { ICar } from '../../interfaces/car';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-car',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './car.component.html',
   styleUrl: './car.component.css'
 })
 export class CarComponent {
+  @Input() carData:ICar|undefined;
+  public carImageWidth:number=300
 
+  constructor (private _carApiService:CarApiService) {}
+
+  deleteCar(carId:string) { 
+    this._carApiService.delCarDetails(carId).subscribe(result =>
+      { 
+        console.log(result);
+      });
+  }
 }
