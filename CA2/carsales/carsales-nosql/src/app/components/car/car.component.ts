35421d14f880f147434cd6b0c28b2b8acaedc1f7
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-26 12:10:25 +0000

    Added styling

--- CA2/carsales/carsales-nosql/src/app/components/car/car.component.ts
+++ CA2/carsales/carsales-nosql/src/app/components/car/car.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Input } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 import { CarApiService } from '../../service/car-api.service';
 import { ICar } from '../../interfaces/car';
 import { CommonModule } from '@angular/common';
@@ -11,15 +11,17 @@ import { CommonModule } from '@angular/common';
   styleUrl: './car.component.css'
 })
 export class CarComponent {
-  @Input() carData:ICar|undefined;
-  public carImageWidth:number=300
+  @Input() carData: ICar | undefined;
+  @Output() carDeletedEvent = new EventEmitter<string>()
+  @Output() carUpdatedEvent = new EventEmitter<string>()
+  public carImageWidth: number = 250
 
-  constructor (private _carApiService:CarApiService) {}
+  constructor(private _carApiService: CarApiService) { }
 
-  deleteCar(carId:string) { 
-    this._carApiService.delCarDetails(carId).subscribe(result =>
-      { 
-        console.log(result);
-      });
+  deleteCar(carId: string) {
+    this._carApiService.delCarDetails(carId).subscribe(result => {
+      console.log(result);
+      this.carDeletedEvent.emit("car got deleted");
+    });
   }
 }
