057c4708feff2087062a80fcc8965b1fa15a101f
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-26 12:10:56 +0000

    Added editCarDetails method

--- CA2/carsales/carsales-nosql/src/app/service/car-api.service.ts
+++ CA2/carsales/carsales-nosql/src/app/service/car-api.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, catchError, tap } from 'rxjs';
-import { ICar } from '../interfaces/car'; 
+import { ICar } from '../interfaces/car';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ServiceNameService {
   constructor(private httpClient: HttpClient) { }
-  
+
 }
 
 
@@ -16,42 +16,48 @@ export class ServiceNameService {
 export class CarApiService {
 
   private _siteURL = "http://localhost:5050/cars"
-  constructor(private _http:HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
-  getCarDetails():Observable<any> {
-
+  getCarDetails(): Observable<any> {
     return this._http.get<ICar>(this._siteURL)
-    .pipe(
-      tap(data => console.log('car data/error' + JSON.stringify(data))
-    ),
-    catchError(this.handleError)
-    );
+      .pipe(
+        tap(data => console.log('car data/error' + JSON.stringify(data))
+        ),
+        catchError(this.handleError)
+      );
   }
 
+  editCarDetails(car:ICar): Observable<any> {
+    return this._http.put<ICar>(this._siteURL, car)
+      .pipe(
+        tap(data => console.log('car data/error' + JSON.stringify(data))
+        ),
+        catchError(this.handleError)
+      );
+  }
 
-  addCarDetails(car:ICar):Observable<any> {
-   return this._http.post<ICar>(this._siteURL, car)
-   .pipe(
-    tap(data => console.log('add car message/error' + JSON.stringify(data))
-    ),
-    catchError(this.handleError)
-    );
-   }
+  addCarDetails(car: ICar): Observable<any> {
+    return this._http.post<ICar>(this._siteURL, car)
+      .pipe(
+        tap(data => console.log('add car message/error' + JSON.stringify(data))
+        ),
+        catchError(this.handleError)
+      );
+  }
 
-
-   delCarDetails(carId:string):Observable<any> {
-    let deleteURL=this._siteURL+"/"+carId;
+  delCarDetails(carId: string): Observable<any> {
+    let deleteURL = this._siteURL + "/" + carId;
     return this._http.delete(deleteURL)
-    .pipe(
-      tap(data => console.log('del car message/error' + JSON.stringify(data))
-    ),
-    catchError(this.handleError)
-    );
+      .pipe(
+        tap(data => console.log('del car message/error' + JSON.stringify(data))
+        ),
+        catchError(this.handleError)
+      );
 
   }
 
 
-  private handleError (err:HttpErrorResponse) {
+  private handleError(err: HttpErrorResponse) {
     console.log('CarApiService: ' + err.message);
     return err.message;
   }
